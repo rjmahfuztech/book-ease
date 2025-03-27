011bacd05f369c22a633eeba7074f1072045cd70
rjmahfuztech <rjmahfuztech@gmail.com>
2025-03-28 00:14:16 +0600

    Custom Member model added and changed username to email

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from users.managers import CustomUserManager
 
-# Create your models here.
+class Member(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    membership_date = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
