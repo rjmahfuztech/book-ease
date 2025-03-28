d60867c057ae6e51eaf0c68dbd9b6819a3560b61
rjmahfuztech <rjmahfuztech@gmail.com>
2025-03-28 14:12:17 +0600

    borrowRecord model added

--- borrow/models.py
+++ borrow/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from books.models import Book
+from users.models import Member
 
-# Create your models here.
+class BorrowRecord(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='borrow')
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='borrow')
+    borrow_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.book.title} borrowed by {self.member.first_name}"
