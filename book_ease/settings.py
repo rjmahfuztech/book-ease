dd28151a85b575cf92af92f559e594a0cb5d2a18
rjmahfuztech <rjmahfuztech@gmail.com>
2025-03-28 14:20:10 +0600

    django debug toolbar added

--- book_ease/settings.py
+++ book_ease/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'books',
     'borrow',
     'users',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -69,6 +71,12 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 WSGI_APPLICATION = 'book_ease.wsgi.application'
