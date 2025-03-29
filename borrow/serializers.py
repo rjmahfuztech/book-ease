f312640850d742515dd5eae9eb2d2f2cab5bab4d
rjmahfuztech <rjmahfuztech@gmail.com>
2025-03-29 13:55:59 +0600

    borrow and return permission setup done

--- borrow/serializers.py
+++ borrow/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from borrow.models import BorrowRecord
 from books.models import Book
 from books.serializers import AuthorSerializer
+from users.models import Member
 
 class SimpleBookSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True)
@@ -9,13 +10,27 @@ class SimpleBookSerializer(serializers.ModelSerializer):
         model = Book
         fields = ['id', 'title', 'author', 'category']
 
+class SimpleMemberSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(method_name='get_current_user_full_name')
+    class Meta:
+        model = Member
+        fields = ['id', 'name']
+
+    def get_current_user_full_name(self, obj):
+        return obj.get_full_name()
+
+
 class BorrowRecordSerializer(serializers.ModelSerializer):
     book_id = serializers.IntegerField(write_only=True)
     book = SimpleBookSerializer(read_only=True)
+    member = serializers.SerializerMethodField(method_name='get_user')
     class Meta:
         model = BorrowRecord
         fields = ['id', 'book', 'book_id', 'member', 'borrow_date', 'return_date']
-        read_only_fields = ['book', 'return_date']
+        read_only_fields = ['book', 'member', 'return_date']
+
+    def get_user(self, obj):
+        return SimpleMemberSerializer(obj.member).data
 
     def validate_book_id(self, value):
         if value <= 0:
--- borrow/views.py
+++ borrow/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets, response, status
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from borrow.serializers import BorrowRecordSerializer, EmptySerializer
 from borrow.models import BorrowRecord
 from rest_framework.decorators import action
@@ -8,15 +9,25 @@ from django.utils import timezone
 
 class BorrowRecordViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'head', 'delete', 'options']
-    queryset = BorrowRecord.objects.select_related('book__author').all()
-
+    
     def get_serializer_class(self):
         if self.action == 'return_book':
             return EmptySerializer
         return BorrowRecordSerializer
+    
+    def get_permissions(self):
+        if self.action in ['list', 'create', 'destroy']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+
+    
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return BorrowRecord.objects.select_related('book__author', 'member').all()
+        return BorrowRecord.objects.select_related('book__author', 'member').filter(member=self.request.user, return_date__isnull=True)
 
     def perform_create(self, serializer):
-        borrow_record = serializer.save()
+        borrow_record = serializer.save(member=self.request.user)
         book = borrow_record.book
         book.availability_status = False
         book.save()
@@ -28,6 +39,9 @@ class BorrowRecordViewSet(viewsets.ModelViewSet):
             borrow_record = BorrowRecord.objects.get(id=pk, return_date__isnull=True)
         except BorrowRecord.DoesNotExist:
             return response.Response(f"Sorry! This borrow record not found or already returned!")
+        
+        if borrow_record.member != request.user:
+            return response.Response({'error': 'You can only return books you borrowed!'}, status=status.HTTP_403_FORBIDDEN)
         
         borrow_record.return_date = timezone.now()
         borrow_record.save()
