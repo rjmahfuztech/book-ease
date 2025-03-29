cb1d434cc042c5a5717352edf103833eb01d64fe
rjmahfuztech <rjmahfuztech@gmail.com>
2025-03-30 00:01:11 +0600

    book isbn validated

--- books/serializers.py
+++ books/serializers.py
@@ -12,15 +12,24 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'author', 'author_id', 'isbn', 'category', 'availability_status']
+        read_only_fields = ['availability_status']
 
     def validate_author_id(self, value):
         if value <= 0:
             raise serializers.ValidationError('Author id must be 1 or bigger')
         elif not Author.objects.filter(pk=value).exists():
             raise serializers.ValidationError(f'Author with Id {value} does not exists')
-        
         return value
     
+    def validate_isbn(self, value):
+        if self.instance: # when try to update
+            if Book.objects.exclude(id=self.instance.id).filter(isbn=value).exists():
+                raise serializers.ValidationError('A book with this ISBN already exists.')
+        else:
+            if Book.objects.filter(isbn=value).exists():
+                raise serializers.ValidationError('A book with this ISBN already exists.')
+        return value
+
     def create(self, validated_data):
-        author_id = validated_data['author_id']
-        return Book.objects.create(author_id=author_id)
+        author_id = validated_data.pop('author_id')
+        return Book.objects.create(author_id=author_id, **validated_data)
