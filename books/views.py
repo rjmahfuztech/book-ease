886ce5fb5e0291bd43ad6af02653bdd0221208dd
rjmahfuztech <rjmahfuztech@gmail.com>
2025-03-29 14:19:27 +0600

    Author and Book model permission added

--- books/views.py
+++ books/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from books.serializers import AuthorSerializer, BookSerializer
 from rest_framework import viewsets
 from books.models import Author, Book
+from api.permissions import IsAdminOrReadOnly
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.select_related('author').all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
