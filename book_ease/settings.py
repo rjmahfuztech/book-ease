55556abb7fd433614a9f97305b4e97708dcf24ae
rjmahfuztech <rjmahfuztech@gmail.com>
2025-04-01 21:56:30 +0600

    Database change to postgrsql

--- book_ease/settings.py
+++ book_ease/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-rpqh!=iz*9s*z&j7efj7+k2m=8^v^hpd!+)5=&pv@k1aej8-!v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 AUTH_USER_MODEL = 'users.Member'
 
@@ -74,16 +74,27 @@ INTERNAL_IPS = [
     # ...
 ]
 
-WSGI_APPLICATION = 'book_ease.wsgi.application'
+WSGI_APPLICATION = 'book_ease.wsgi.app'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('dbname'),
+        'USER': config('user'),
+        'PASSWORD': config('password'),
+        'HOST': config('host'),
+        'PORT': config('port')
     }
 }
 
